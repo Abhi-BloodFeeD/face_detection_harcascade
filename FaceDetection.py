9236356539c1d72914495a4caa87ce4169efd403
Abhi-BloodFeeD <abhinavmetta@gmail.com>
2022-12-04 13:34:05 +0530

    cleaning

--- FaceDetection.py
+++ FaceDetection.py
@@ -13,6 +13,7 @@ while True:
     face_coordinates = training_face_data.detectMultiScale(grayscaled_img)
     # print(face_coordinates)
     for (x, y, w, h) in face_coordinates:
+
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
     cv2.imshow("FACE DETECTOR BY ABHINAV BHARDWAJ", frame)
     key = cv2.waitKey(1)
