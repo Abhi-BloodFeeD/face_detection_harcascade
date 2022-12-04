d15fb949ffd46f625f4f64ef62d644690e75d9d7
Abhi-BloodFeeD <abhinavmetta@gmail.com>
2022-12-04 09:54:11 +0530

    FACE DETECTION APP

---
+++ FaceDetection.py
@@ -0,0 +1,21 @@
+import cv2
+
+training_face_data = cv2.CascadeClassifier(
+    'haarcascade_frontalface_default.xml')
+
+# img = c v2.imread('im1.webp')
+webcam = cv2.VideoCapture(0)
+while True:
+    successfull_fame, frame = webcam.read()
+
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Detect Faces
+    face_coordinates = training_face_data.detectMultiScale(grayscaled_img)
+    # print(face_coordinates)
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.imshow("FACE DETECTOR BY ABHINAV BHARDWAJ", frame)
+    key = cv2.waitKey(1)
+    if key == 81 or key == 113:
+        break
+webcam.release()
